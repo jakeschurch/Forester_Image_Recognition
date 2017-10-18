116a6f06575baeca73ebf680e840e4337c9ebc46
jakeschurch <jschurch@bryant.edu>
2017-10-18 00:24:22 -0400

    Add functions to Download and extract graph from web

--- bot.py
+++ bot.py
@@ -13,8 +13,8 @@ to that object
 -send images to server - rpc -> remote procedure call: use rpyc package
 
 server-side:
-    rpyc sets server-sideon client side create connection to localhsost given ip
-        address of robot
+    rpyc sets server-sideon client side create connection to localhost given
+    ip address of robot
     access imports module os and calls os functions
 
 intermediary file for photos?
@@ -23,30 +23,35 @@ laptop as part of the robot?
 
 -- Project TODO's:
 TODO: Set up image recognition model to work starting with laptop
+REVIEW: Download fswebcam on debian distro.
 TODO: Set up tether between laptop and forester. NOTE: may want to use bash.
 TODO: Set up rpyc instance between laptop and forester.
 TODO: Implement image recognition model using rpyc instance on forester.
+
+QUESTION: Change file to bytestream?
+QUESTION: Define folder paths for images and model?
 """
 
 __author__ = "Jake Schurch"
 __version__ = "0.0.1a"
 
-import tensorflow as tf
-import rpyc
 import os
 import tarfile
-import urllib
+import urllib.request
+
+# import rpyc
+# import tensorflow as tf
 
 
 class Camera(object):
-    picnum = 0
 
     def __init__(self):
-        pass
+        self.picnum = 0
 
     def TakePicture(self):
         try:
-            os.system(f"fswebcam -r 352x288 -no-banner {picnum}.jpg")
+            os.system(
+                f"fswebcam -r 352x288 -no-banner image_{self.picnum}.jpg")
             self.picnum += 1
         except OSError:
             Exception("Not valid.")
@@ -54,29 +59,35 @@ class Camera(object):
 
 class Model(object):
 
-    def __init__(self, framework: str):
-        if "server" in framework:
-            self.InitModel
-        else:
-            pass
+    def __init__(self):
+        self.BaseURL = "http://download.tensorflow.org/models/object_detection"
 
-    def InitModel(self):
-        ModelBaseURL = "http://download.tensorflow.org/models/object_detection"
-        ModelFile = "ssd_inception_v2_coco_11_06_2017.tar.gz"
-        FullURL = ModelBaseURL + "/" + ModelFile
+        self.ModelFile = r"ssd_inception_v2_coco_11_06_2017.tar.gz"
 
-        if not os.path.exists(f"/{ModelName}"):
-            os.system(f"wget -O {FullURL}")
-            tar_file = tarfile.open(ModelFile)
+    def Download(self) -> None:
+        """Download imagenet model from web."""
 
-            # Load Frozen Model
-            for file in tar_file.getmembers():
-                if "frozen_inference_graph.pb" in file.name:
-                    tar_file.extract(file, os.getcwd())
+        FullURL = self.BaseURL + "/" + self.ModelFile
 
-        def SetToActive(self):
+        opener = urllib.request.URLopener()
+        opener.retrieve(FullURL, self.ModelFile)
 
+    def ExtractGraph(self) -> None:
+        """Extract model from tar zip file."""
+
+        tar_file = tarfile.open(ModelFile)
+        for file in tar_file.getmembers():
+            if "frozen_inference_graph.pb" in file.name:
+                tar_file.extract(file, os.getcwd())
+
+    def LoadGraph(self):
+        return NotImplementedError
+
+    def LoadLabels(self):
+        return NotImplementedError
 
 
 if __name__ == "__main__":
-    pass
+    m = Model()
+    m.Download()
+    m.Extract()
